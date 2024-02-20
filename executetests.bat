a2014e40d6008b8a0fc641f52312bcd3aad0f837
Camille GAYAT <camille.gayat@algosup.com>
2024-02-20 09:26:56 +0100

    changing .bat file to .cmd

--- executetests.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-cd interpreter
-gcc -o main main.c
-main tests/memoryOverflow.all ../binary/output.bin
-cd ..
-cd binary
-gcc -o binaryConverter binaryConverter.c
-binaryConverter output.bin
-PAUSE
