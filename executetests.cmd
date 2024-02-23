132b91623cc712a185ba54f4a9477815e57a09cd
raproudhon <raphael.proudhon@algosup.com>
2024-02-23 12:05:13 +0100

    Update executetests.cmd

--- executetests.cmd
+++ executetests.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 cd interpreter
 gcc -o main main.c
-main tests/syntacticalError.all ../binary/output.bin
+main testMC.all ../binary/output.bin
 cd ..
 cd binary
 gcc -o binaryConverter binaryConverter.c
